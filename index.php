ebbe896bedb0759f9098a8d81ab47f8dc99e1788
paulgenesisbacuna@gmail.com <paulgenesisbacuna@gmail.com>
2019-09-04 11:28:38 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home | Preventive Quality Program</title>
+	<link rel="bootstrap/css/bootstrap.min.css" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="pql_css/home.css">
+	<script src="bootstrap/js/bootstrap.min.js" crossorigin="anonymous"></script>
+</head>
+<body class="bg">
+	<div class="my-container">
+		<h1>Sample</h1>
+	</div>
+</body>
+</html>
