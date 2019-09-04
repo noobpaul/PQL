b9196fc706f2848679898b4f867d14d849c54d31
paulgenesisbacuna@gmail.com <paulgenesisbacuna@gmail.com>
2019-09-04 18:07:41 +0800

    set background-img

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Home | Preventive Quality Program</title>
+	<title>Home | Machine Predictive Quality</title>
 	<link rel="bootstrap/css/bootstrap.min.css" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="pql_css/home.css">
 	<script src="bootstrap/js/bootstrap.min.js" crossorigin="anonymous"></script>
 </head>
 <body class="bg">
-	<div class="my-container">
-		<h1>Sample</h1>
-	</div>
+	<div class="header"></div>
+	<!-- <div class="bg"></div> -->
 </body>
 </html>
